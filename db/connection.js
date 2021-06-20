a6a3edd8ceac065c530c93747dbb5488391e1939
Deepika Bekal <deepika.bekal2@gmail.com>
2021-06-20 15:57:02 +0400

    env file added

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,12 @@
 const mysql = require ('mysql2');
-
+require('dotenv').config();
 const db = mysql.createConnection(
     {
       host: 'localhost',
       // Your MySQL username,
       user: 'root',
       // Your MySQL password
-      password: 'pass',
+      password: process.env.MYSQL_PASS,
       database: 'employees_tracker'
     },
     console.log('Connected to the Employees_Tracker database.')
